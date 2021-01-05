7ca4a7eac7ed7bc95322ce93b642a47387d9cc8a
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-05 15:15:59 +0100

    Structure de contrôle - if - twig
    Decouverte du if else if
    Interpolation d'une image

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use stdClass;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -14,21 +14,32 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        //je déclare une variable php de type string
-        $author = "Loïs Lane";
-        // j'instancie un objet standard PHP sans classe fait par nous-même
-        // je n'oublie pas l'import  | NameSpaceResolver -> plugin
-        $article = new stdClass();
-        //j'attribue des propriétés à mon objet
-        $article->title = "Théorie du complot";
-        $article->intro = "Fascine depuis des lustres ! On vous dit tout";
-        $article->content = "Bla bla bla, Pa pa pa, Po po po";
+        // je déclare une variable php de type string 
+        $author= "Loïs Lane";
 
-        // j'envoie tout ça à ma vue pour l'afficher 
+        // J'instancie un objet standard PHP sans class fait pas nous meme
+        // Je n'oublie pas l'import | NameSpaceResolver -> plugin
+        $article = new stdClass();
+        // J'attribue des proprietes à mon objet
+        $article->title = "Théorie du complot";
+        $article->intro = "Fascine depuis des lustres ! on vous dit tout";
+        $article->content = "Blabla, Po po po , papa papa bla bla";
+
+        // J'instancie un autre objet
+        $michel = new stdClass();
+        $michel->name = "Michel";
+        $michel->age = 58;
+
+        $outkast = "JD3000";
+        $picture = "";
+        // Je file tout ça à ma vue pour l'afficher !
         return $this->render('home/index.html.twig', [
             'name' => 'Page d\'accueil',
             "article" => $article,
-            "auteur" => $author
+            "auteur"  => $author,
+            "user"    => $michel,
+            "jeanDaniel" =>  $outkast
+            
         ]);
     }
 }
