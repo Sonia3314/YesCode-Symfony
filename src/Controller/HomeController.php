b45766e5c2f2bd8db36e899a14e451de14469cf8
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-05 16:45:52 +0100

    boucle for classique de 1 à 10
    boucle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,19 +14,12 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        //ici on déclare un tableau
-        $games = [
-            "Starcraft 2" => 8,
-            "BF6" => 128,
-            "Metro Exodus" => 1,
-            "Tekken 5" => 2,
-            "Crash Bash" => 4 
-        ];
+
 
 
         return $this->render('home/index.html.twig', [
             'name' => 'Page d\'accueil',
-            "games" => $games
+      
 
 
         ]);
