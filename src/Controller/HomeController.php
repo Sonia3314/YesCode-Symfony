4ea7d56b7fe5d4709e1f11b83d518310f67489cf
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-05 14:28:05 +0100

    Interpolation
    Interpolation de la vue au controller avec objet er var de type string ou integer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,11 +14,21 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        // je declare un name qui sera envoyé à la vue
-        // et qui contient -> Page d'accueil- dans la vue j'utilise 
-        // la var name
+        //je déclare une variable php de type string
+        $author = "Loïs Lane";
+        // j'instancie un objet standard PHP sans classe fait par nous-même
+        // je n'oublie pas l'import  | NameSpaceResolver -> plugin
+        $article = new stdClass();
+        //j'attribue des propriétés à mon objet
+        $article->title = "Théorie du complot";
+        $article->intro = "Fascine depuis des lustres ! On vous dit tout";
+        $article->content = "Bla bla bla, Pa pa pa, Po po po";
+
+        // j'envoie tout ça à ma vue pour l'afficher 
         return $this->render('home/index.html.twig', [
-            'name' => 'HomeController',
+            'name' => 'Page d\'accueil',
+            "article" => $article,
+            "auteur" => $author
         ]);
     }
 }
