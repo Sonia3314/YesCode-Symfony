a6eb24b5173e7aed44c3d1d6e2020696529707c0
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-07 16:24:45 +0100

    suppression de l'entité
    grace aux commandes de migration dans le terminal, création d'une entité fruit dans une bdd

--- migrations/Version20210107152230.php
+++ migrations/Version20210107152230.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210107150252 extends AbstractMigration
+final class Version20210107152230 extends AbstractMigration
 {
     public function getDescription() : string
     {
