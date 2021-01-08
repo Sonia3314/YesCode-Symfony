b31ec04fe31dde291f3123cd4feda6b90da57f8d
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-08 10:17:58 +0100

    recuperation des données en bdd
    recuperation des données via le controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use App\Entity\Fruit;
+use App\Repository\FruitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,26 +15,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function index(EntityManagerInterface $manager): Response
+    public function index(FruitRepository $fruitRepository): Response
     {
-        $banane = new Fruit();
-        $fraise = new Fruit();
+        $fruits = $fruitRepository->findAll();
+        dump($fruits);
 
-        $banane->setName("banane des Antilles");
-        $fraise->setName("fraise des bois");
+        return $this->render('home/index.html.twig', [
+            "fruits"=> $fruits
 
-        
-        //dit à doctrine que tu veux sauvegarder (éventuellement) le produit
-        $manager->persist($banane);
-        $manager->persist($fraise);
-
-        // Un seul Flush pour tous les persist ! envoie ce qui est persist ou autre
-        //mais on fait souvent un persist à la fois
-        //execute les requetes 
-        $manager->flush();
-        //le tout se déclenche en actualisant la page 
-
-        return $this->render('home/index.html.twig', []);
+        ]);
 
     }
 }
