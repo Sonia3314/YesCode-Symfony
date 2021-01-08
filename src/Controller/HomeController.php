2cc00de013eb49f5caf55467342a7cfc591b74df
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-08 09:54:44 +0100

    Test supplémentaire Fruit persist / flush
    ajout de commentaire concernant persist et flush

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Fruit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,15 +14,26 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $manager): Response
     {
-        return $this->render('home/index.html.twig', [
-        ]);
+        $banane = new Fruit();
+        $fraise = new Fruit();
+
+        $banane->setName("banane des Antilles");
+        $fraise->setName("fraise des bois");
+
+        
+        //dit à doctrine que tu veux sauvegarder (éventuellement) le produit
+        $manager->persist($banane);
+        $manager->persist($fraise);
+
+        // Un seul Flush pour tous les persist ! envoie ce qui est persist ou autre
+        //mais on fait souvent un persist à la fois
+        //execute les requetes 
+        $manager->flush();
+        //le tout se déclenche en actualisant la page 
+
+        return $this->render('home/index.html.twig', []);
+
     }
-
 }
-
-
-
-
-
