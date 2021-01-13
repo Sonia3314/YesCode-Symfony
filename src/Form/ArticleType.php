7b15a1d5b6639df35d10dc579323b12935a12dec
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-13 14:40:17 +0100

    Ajout de label et placeholder
    sur les differents inputs

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,21 +4,37 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('intro')
-            ->add('content')
-            ->add('Image')
+            ->add('title', TextType::class , [
+            'label' => 'Titre de votre article',
+            'attr' =>  ['placeholder' => 'Tapez votre titre ici ']
+                ])
+            ->add('intro', TextType::class , [
+                'label' => 'Intro de votre article',
+                'attr' =>  ['placeholder' => 'Une phrase d\'accroche', 'class' => 'bg-dark']
+                    ])
+            ->add('content', TextareaType::class , [
+                'label' => 'Votre contenu',
+                'attr' =>  ['placeholder' => 'Dites-nous tout !']
+                    ])
+            ->add('Image', UrlType::class , [
+                'label' => 'Adresse de l\'image',
+                'attr' =>  ['placeholder' => 'Collez un lien d\'image']
+                    ])
             ->add('Envoyer', SubmitType::class)
-        ;
+            ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
