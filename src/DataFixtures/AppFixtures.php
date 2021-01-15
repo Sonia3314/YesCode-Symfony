7c5edb8c23d5ea8d01b71e1253c178a512e9808a
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-01-15 14:46:40 +0100

    Article + Auteur + Fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,26 +14,9 @@ class AppFixtures extends Fixture
     {
         $faker = Faker\Factory::create('fr_FR');
 
-        // GESTION DES ARTICLES
-        for ($i=0; $i <=20 ; $i++) { 
-            
-            $article = new Article();
-            
-            $title = $faker->sentence(2);
+    // GESTION DES USERS - création de faux utlisateurs
 
-            $image = "https://picsum.photos/400/300";
-
-            $intro = $faker->paragraph(2);
-
-            $content = '<p>' . implode('</p><p>',$faker->paragraphs(5)) . '</p>';
-
-            $article->setTitle($title)
-                    ->setImage($image)
-                    ->setIntro($intro)
-                    ->setContent($content);
-
-            $manager->persist($article);
-            }
+        $users = [];
 
         $genres = ['male', 'female'];
 
@@ -55,8 +38,36 @@ class AppFixtures extends Fixture
                  ->setHash("password");
         
             $manager->persist($user);
-
+            //Ici je charge mon tableau
+            $users[] = $user;
+      
         }
+
+        // GESTION DES ARTICLES
+
+        for ($i=0; $i <=20 ; $i++) { 
+            
+            $article = new Article();
+            
+            $title = $faker->sentence(2);
+
+            $image = "https://picsum.photos/400/300";
+
+            $intro = $faker->paragraph(2);
+
+            $content = '<p>' . implode('</p><p>',$faker->paragraphs(5)) . '</p>';
+
+            //J'utilise mes users pour en prendre au hasard 
+            $author= $users[mt_rand(0, count($users) -1)];
+
+            $article->setTitle($title)
+                    ->setImage($image)
+                    ->setIntro($intro)
+                    ->setContent($content)
+                    ->setAuthor($author);
+
+            $manager->persist($article);
+            }
 
         $manager->flush();
 
@@ -66,4 +77,3 @@ class AppFixtures extends Fixture
     }
 
 }
-
